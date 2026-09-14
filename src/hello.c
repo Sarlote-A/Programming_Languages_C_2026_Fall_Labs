967e94a4c207e563235b585f991533be4b606bc5
Sarlote-A <dzoanna.akselrode@edu.rtu.lv>
2026-09-14 08:11:53 +0000

    exchanges greetings

--- src/hello.c
+++ src/hello.c
@@ -1,11 +1,12 @@
 
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
   // This is your first C program my friend
-  printf("Hello, RTU World from C Lab in 2025!\n");
+     printf("Hello from <Your Name>! \n");
+    //  printf("Hello, RTU World from C Lab in 2025!\n");
   printf("You passed %d argument(s).\n", argc - 1);
-  for (int i = 1; i < argc; ++i) {
+  for (int i = 1; i < argc; ++i){
     printf("  arg[%d] = %s\n", i, argv[i]);
   }
   return 0;
